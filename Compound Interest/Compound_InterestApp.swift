712055b6f88c51068608a3ec0a209be33d85277e
Bogdan Kostyuchenko <b.kostyuchenko@tinkoff.ru>
2025-12-04 17:51:54 +0300

    Initial Commit

---
+++ Compound Interest/Compound_InterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compound_InterestApp.swift
+//  Compound Interest
+//
+//  Created by Bogdan Kostyuchenko on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Compound_InterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
